ac53fd947337be04b5f74a66ee4b8ccff15b83d4
pyush-18 <piyushjoshi1812@gmail.com>
2026-05-12 14:28:24 +0530

    chore: configure backend for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -36,6 +36,11 @@ app.get("/", (req, res) => {
 app.use("/api", emailRouter);
 app.use("/api/payment", require("./routes/paymentRoute"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port: ${process.env.PORT}`);
-});
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+}
+
+module.exports = app;
